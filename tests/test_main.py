22549918f55d004073fbd4b238f2f08d4cff2293
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2022-03-10 09:15:46 +0700

    :sun:

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,19 @@
+import os
 import pytest
+import subprocess
 
 from hello import hello
 
-@pytest.mark.integration
+@pytest.fixture(scope="module", autouse=True)
+def prepare_db():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    setup_db_script = os.path.join(dir_path, "setup_db.sh")
+
+    print("preparing db")
+    subprocess.run(setup_db_script)
+    print("db is ready")
+    yield
+
 def test_hello():
     got = hello("Arief")
     assert got == "Hello, Arief!"
